3ed91114b2db00d99135bfd1d5f74c1920c1e5d1
Jan Borchers <humancatfood@users.noreply.github.com>
2020-03-22 11:34:58 +0000

    add some tests

--- test/simple_test.js
+++ test/simple_test.js
@@ -1,5 +1,139 @@
+/* eslint-disable padded-blocks, arrow-body-style, arrow-parens */
+const { getProcessingPage } = require('../src/index.js');
+
+
 describe('simple', () => {
-  it('should be sane', () => {
-    expect(false).not.toBe(true);
+
+  it('should succeed', () => {
+
+    return getProcessingPage([{
+      state: 'success',
+    }])
+      .then(result => expect(result).toEqual({
+        title: 'Order complete',
+        message: null,
+      }));
+
   });
+
+  it('should ignore errorCode in success case', () => {
+
+    return Promise.all([
+
+      getProcessingPage([{
+        state: 'success',
+        errorCode: 'NO_STOCK',
+      }])
+        .then(result => expect(result).toEqual({
+          title: 'Order complete',
+          message: null,
+        })),
+
+      getProcessingPage([{
+        state: 'success',
+        errorCode: null,
+      }])
+        .then(result => expect(result).toEqual({
+          title: 'Order complete',
+          message: null,
+        })),
+
+      getProcessingPage([{
+        state: 'success',
+        errorCode: 'some other string',
+      }])
+        .then(result => expect(result).toEqual({
+          title: 'Order complete',
+          message: null,
+        })),
+
+    ]);
+
+
+  });
+
+
+  it('should succeed immediately', () => {
+
+    const start = Date.now();
+
+    return getProcessingPage([{
+      state: 'success',
+    }])
+      .then(() => expect(Date.now() - start).toBeCloseTo(0));
+
+  });
+
+
+  it('should succeed eventually', () => {
+
+    const start = Date.now();
+
+    return Promise.all([
+
+      getProcessingPage([{
+        state: 'processing',
+      }, {
+        state: 'success',
+      }])
+        .then(() => expect(Date.now() - start).toBeCloseTo(2000)),
+
+      getProcessingPage([{
+        state: 'processing',
+      }, {
+        state: 'processing',
+      }, {
+        state: 'success',
+      }])
+        .then(() => expect(Date.now() - start).toBeCloseTo(4000)),
+
+    ]);
+
+  });
+
+
+  it('should error out', () => {
+
+    return Promise.all([
+
+      getProcessingPage([{
+        state: 'error',
+        errorCode: 'NO_STOCK',
+      }])
+        .then(result => expect(result).toEqual({
+          title: 'Error page',
+          message: 'No stock has been found',
+        })),
+
+      getProcessingPage([{
+        state: 'error',
+        errorCode: 'INCORRECT_DETAILS',
+      }])
+        .then(result => expect(result).toEqual({
+          title: 'Error page',
+          message: 'Incorrect details have been entered',
+        })),
+
+      getProcessingPage([{
+        state: 'error',
+        errorCode: undefined,
+      }])
+        .then(result => expect(result).toEqual({
+          title: 'Error page',
+          message: null,
+        })),
+
+      getProcessingPage([{
+        state: 'error',
+        errorCode: null,
+      }])
+        .then(result => expect(result).toEqual({
+          title: 'Error page',
+          message: null,
+        })),
+
+    ]);
+
+  });
+
 });
